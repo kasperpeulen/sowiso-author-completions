b771b142a577ee40b0737d7a3d28f3034ce3dfdb
Kasper Peulen <kasperpeulen@gmail.com>
2016-07-20 20:19:32 +0200

    refactor: rename reducer_completions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@
 import { combineReducers } from 'redux';
 import showCompletions from './reducer_show_completions';
 import completionPosition from './reducer_completion_position';
-import completions from './reducer_all_completions';
+import completions from './reducer_completions';
 
 const rootReducer = combineReducers({
   showCompletions,
