6bbc46cb93c80a4387b2eeb26474237b13730f77
Kasper Peulen <kasperpeulen@gmail.com>
2016-07-20 14:23:25 +0200

    feat: inject Hello World in sowiso!

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const store = applyMiddleware()(createStore)(reducers);
+
+// The whole extension will live in a div appended to body
+// with an `extension-container` class.
+const createContainerAndAppendToDom = () => {
+  const extensionContainer = document.createElement('div');
+  extensionContainer.className = 'extension-container';
+  document.body.appendChild(extensionContainer);
+  return extensionContainer;
+};
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    , createContainerAndAppendToDom());
+
