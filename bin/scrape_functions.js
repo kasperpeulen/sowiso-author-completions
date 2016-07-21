6c69762f02a5259842cf4efbffdb117ce9d83b25
Kasper Peulen <kasperpeulen@gmail.com>
2016-07-21 15:13:18 +0200

    refactor

--- bin/scrape_functions.js
+++ bin/scrape_functions.js
@@ -1,8 +1,8 @@
 /* @flow */
+import fs from 'fs';
+import path from 'path';
 import {jsdom} from 'jsdom';
 import fsp from 'fs-promise';
-import path from 'path';
-import fs from 'fs';
 
 // get the file from https://cloud.sowiso.nl/docs/exercise_manual
 // this file is not checked in
@@ -10,8 +10,8 @@ const inputFile = 'bin/exercise_manual.html';
 const outputDir = 'src/resources';
 const outputFile = 'sowiso_php_functions.json';
 
-async function getSowisoPhpFunctions() {
-  if (!workingDirectoryIsRootOfPackage()) {
+(async() => {
+  if (!await workingDirectoryIsRootOfPackage(process.cwd())) {
     console.log("Run this script from the root folder, where package.json lives");
     return;
   }
@@ -21,8 +21,26 @@ async function getSowisoPhpFunctions() {
     html = await fsp.readFile(inputFile, 'utf8');
   } catch (err) {
     console.log(err);
+    return;
   }
 
+  const json = turnRawHtmlIntoJson(html);
+
+  if (!await fsp.exists(outputDir)) {
+    await fsp.mkdir(outputDir);
+  }
+
+  try {
+    const outputPath = path.join(outputDir, outputFile);
+    await fsp.writeFile(outputPath, JSON.stringify(json));
+    console.log(`Created ${outputPath}`);
+  } catch (e) {
+    console.log(e);
+  }
+})();
+
+// pure function
+function turnRawHtmlIntoJson(html: String): [{completion: string, description: string}] {
   const window = jsdom(html).defaultView;
 
   const headings = window.document.querySelectorAll('h3');
@@ -34,7 +52,7 @@ async function getSowisoPhpFunctions() {
   const tbody = table.firstElementChild.nextElementSibling;
   const trChilds = tbody.children;
 
-  const results = [].map.call(trChilds, (tr) => {
+  return [].map.call(trChilds, (tr) => {
     const completion = tr.firstElementChild.textContent;
     const description = tr.firstElementChild.nextElementSibling.textContent;
     return {
@@ -42,27 +60,14 @@ async function getSowisoPhpFunctions() {
       description
     };
   });
-
-  if (!fs.existsSync(outputDir)){
-    fs.mkdirSync(outputDir);
-  }
-
-  try {
-    await fsp.writeFile(path.join(outputDir, outputFile), JSON.stringify(results));
-  } catch(e) {
-    console.log(e);
-  }
-
 }
 
-function workingDirectoryIsRootOfPackage(): boolean {
+async function workingDirectoryIsRootOfPackage(workingDirectory: string): Promise<boolean> {
   try {
-    const packagePath = path.join(process.cwd(), 'package.json');
-    fs.accessSync(packagePath, fs.F_OK);
+    const packagePath = path.join(workingDirectory, 'package.json');
+    await fsp.access(packagePath, fs.F_OK);
     return true;
   } catch (e) {
     return false;
   }
 }
-
-getSowisoPhpFunctions();
