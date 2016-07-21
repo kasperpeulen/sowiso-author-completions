a3d1ca35c5446e2954f7310481ab3fad1f947664
Kasper Peulen <kasperpeulen@gmail.com>
2016-07-21 02:28:10 +0200

    chore: improve redux types

--- flow-typed/npm/redux_v3.x.x.js
+++ flow-typed/npm/redux_v3.x.x.js
@@ -1,6 +1,7 @@
 // flow-typed signature: 3b60d0484a561f2e7c43ac2a5eb28d87
 // flow-typed version: f622abbe3e/redux_v3.x.x/flow_>=v0.23.x
 
+// hand edited by kasperpeulen
 declare module 'redux' {
   declare type State = any;
   declare type Action = Object;
@@ -11,22 +12,22 @@ declare module 'redux' {
   declare type ActionCreator = (...args: any) => Action | AsyncAction;
   declare type MiddlewareAPI = { dispatch: Dispatch, getState: () => State };
   declare type Middleware = (api: MiddlewareAPI) => (next: Dispatch) => Dispatch;
-  declare type Store = {
+  declare type Store<S> = {
     dispatch: Dispatch,
-    getState: () => State,
+    getState: () => S,
     subscribe: (listener: () => void) => () => void,
-    replaceReducer: (reducer: Reducer<any, any>) => void
+    replaceReducer: (reducer: Reducer<S, Action>) => void
   };
-  declare type StoreCreator = (reducer: Reducer<any, any>, initialState: ?State) => Store;
-  declare type StoreEnhancer = (next: StoreCreator) => StoreCreator;
+  declare type StoreCreator<S> = (reducer: Reducer<S, Action>, initialState?: S) => Store<S>;
+  declare type StoreEnhancer<S> = (next: StoreCreator<S>) => StoreCreator<S>;
   declare type ActionCreatorOrObjectOfACs = ActionCreator | { [key: string]: ActionCreator };
   declare type Reducers = { [key: string]: Reducer<any, any> };
   declare class Redux {
   bindActionCreators<actionCreators: ActionCreatorOrObjectOfACs>(actionCreators: actionCreators, dispatch: Dispatch): actionCreators;
   combineReducers(reducers: Reducers): Reducer<any, any>;
-  createStore(reducer: Reducer<any, any>, initialState?: State, enhancer?: StoreEnhancer): Store;
-  applyMiddleware(...middlewares: Array<Middleware>): StoreEnhancer;
-  compose(...functions: Array<Function | StoreEnhancer>): Function;
+  createStore<S>(reducer: Reducer<S, Action>, initialState?: S, enhancer?: StoreEnhancer<S>): Store<S>;
+  applyMiddleware<S>(...middlewares: Array<Middleware>): StoreEnhancer<S>;
+  compose<S>(...functions: Array<Function | StoreEnhancer<S>>): Function;
   }
   declare var exports: Redux;
 }
